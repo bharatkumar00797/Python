f22096a6dd7765fc310c19f168c1682510e623dd
Bharatkumar Chandvani <154852574+bharatkumar00797@users.noreply.github.com>
2024-05-07 23:12:01 -0400

    Update solutions.py

--- Everyday Leetcode solutions streak Day 3/solutions.py
+++ Everyday Leetcode solutions streak Day 3/solutions.py
@@ -1,1 +1,28 @@
 
+class Solution(object):
+    def findRelativeRanks(self, score):
+        """
+        :type score: List[int]
+        :rtype: List[str]
+        """
+        # Sort the scores in descending order
+        sorted_scores = sorted(score, reverse=True)
+        rank_dict = {}
+        
+        # Assign ranks
+        for i, s in enumerate(sorted_scores):
+            if i == 0:
+                rank_dict[s] = "Gold Medal"  # Assign "Gold Medal" to the highest score
+            elif i == 1:
+                rank_dict[s] = "Silver Medal"  # Assign "Silver Medal" to the 2nd highest score
+            elif i == 2:
+                rank_dict[s] = "Bronze Medal"  # Assign "Bronze Medal" to the 3rd highest score
+            else:
+                rank_dict[s] = str(i + 1)  # Assign the numeric rank to other scores
+        
+        # Create the answer array
+        answer = []
+        for s in score:
+            answer.append(rank_dict[s])  # Append the corresponding rank to the answer array
+        
+        return answer
